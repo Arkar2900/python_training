eda6253cb0efb0b7159f83865f8ea859457b9e6c
Arkar <arkar2900@gmail.com>
2022-04-26 07:15:06 +0630

    #2 make comment to desk

--- tutorials/tutorial_5/day5_akm.py
+++ tutorials/tutorial_5/day5_akm.py
@@ -1,21 +1,23 @@
 # 1
-desk = "C:/Users/Admin/OneDrive/Desktop/Git_ojt/python_training/tutorials/tutorial_5/"
-f = open(desk + "meeting.txt", "w")
-f.write("Nice to meet you. I' m Tendo Soji. I'm from Japan.")
+#desk = "C:/Users/Admin/OneDrive/Desktop/Git_ojt/python_training/tutorials/tutorial_5/"
+f = open("meeting.txt", "w")
+f.write("Nice to meet you. I'm Tendo Soji. I'm from Japan.")
 f.close()
 
 
 # 2
 def count_word(x):
-    f = open(desk + x, "r")
+    f = open(x, "r")
     a_string = f.read()
     word_list = a_string.split()
     num_word = len(word_list)
     print(num_word)
 
 
-# 3
 count_word("meeting.txt")
+
+
+# 3
 number = input("Enter a number: ")
 num = int(number)
 if num < 0:
