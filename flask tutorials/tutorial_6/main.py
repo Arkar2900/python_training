00ce9d3ad01e93c3f3e13d1c170d452b0fdcd9fb
Arkar <arkar2900@gmail.com>
2022-05-19 21:46:03 +0630

    #19 Tutorial_6, in prpgress, create function has some error

--- flask tutorials/tutorial_6/main.py
+++ flask tutorials/tutorial_6/main.py
@@ -5,28 +5,55 @@ from flask_restx import fields, Api, Resource
 app = Flask(__name__)
 api = Api(app)
 model = api.model('Model', {
-    'task': fields.String,
-    'uri': fields.Url('todo_ep')
+    'todo_id': fields.String(readonly=True),
+    'task': fields.String(required=True)
 })
 
 
-class TodoDao(object):
-    def __init__(self, todo_id, task):
-        self.todo_id = todo_id
-        self.task = task
-        # This field will not be sent in the response
-        self.status = 'active'
+class TodoDAO(object):
+    def __init__(self):
+        self.counter = 0
+        self.todos = []
+
+    def get(self, id):
+        for model in self.todos:
+            if model['id'] == id:
+                return model
+        api.abort(404, "Todo {} doesn't exist".format(id))
+
+    def create(self, id, data):
+        model = data
+        model['todo_id'] = id
+        self.todos.append(model)
+        return model
+
+    def update(self, id, data): 
+        model = self.get(id)
+        model.update(data)
+        return model
 
 
 @api.route('/todo')
 class Todo(Resource):
-    @api.marshal_with(model)
-    def get(self, **kwargs):
-        return TodoDao(todo_id='my_todo', task='Remember the milk')
 
-    @api.expect(model, validate=True)
-    def put(self):
-        return 'Test for expect()'
+    @api.marshal_list_with(model)
+    def get(self):
+        '''List all tasks'''
+        return TodoDAO().todos
+
+
+@api.route('/todo/<string:id>')
+class Todo_1(Resource):
+    @api.expect(model)
+    @api.marshal_with(model)
+    def post(self, id):
+        return TodoDAO().create(id, api.payload)
+
+    @api.expect(model)
+    @api.marshal_with(model)
+    def put(self, id):
+        '''Update a task given its identifier'''
+        return TodoDAO().update(id, api.payload)
 
 
 if __name__ == '__main__':
